7016a127e02a1f59f4bf87334e4efde3c12330d1
Christian Shaffer <calexandershaffer@gmail.com>
2023-12-03 09:56:36 -0500

    Add base logic behind playing the game
    No save states yet, but the game is fully playable and doesn't appear to
    have bugs (yet).

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ruby-hangman
+Following The Odin Project, I am tasked with creating a simple command line Hangman game. The extra interesting part is using an external file to a list of 10,000 random words to pick from as well as adding the ability to create save and load states of the game after pausing to resume. 
+
+All the details of the assignment can be found here: https://www.theodinproject.com/lessons/ruby-hangman
---
+++ app.rb
@@ -0,0 +1,149 @@
+class Game
+  @@words = File.read('words.txt').split
+
+  attr_accessor :selected_word, :guessed_letters, :word_so_far, :hangman_states, :wrong_guesses
+
+  def initialize
+    @selected_word = @@words.sample
+    @guessed_letters = []
+    @word_so_far = '_ ' * selected_word.length
+    @hangman_states = [
+      '
+        +---+
+        |   |
+            |
+            |
+            |
+            |
+      =========
+      ',
+      '
+        +---+
+        |   |
+        O   |
+            |
+            |
+            |
+      =========
+      ',
+      '
+        +---+
+        |   |
+        O   |
+        |   |
+            |
+            |
+      =========
+      ',
+      '
+        +---+
+        |   |
+        O   |
+       /|   |
+            |
+            |
+      =========
+      ',
+      %q(
+        +---+
+        |   |
+        O   |
+       /|\  |
+            |
+            |
+      =========
+      ),
+      %q(
+        +---+
+        |   |
+        O   |
+       /|\  |
+       /    |
+            |
+      =========
+      ),
+      %q(
+        +---+
+        |   |
+        O   |
+       /|\  |
+       / \  |
+            |
+      =========
+      )
+    ]
+    @wrong_guesses = 0
+    show_word_progress
+    create_player
+  end
+
+  def create_player
+    player = Player.new(self)
+  end
+
+  def play(player)
+    puts "For debugging, the word is: #{@selected_word}."
+    show_hangman_state
+    puts "Welcome to Hangman. You know how to play. Don't lose please. Start guessing."
+    player.make_guess while @wrong_guesses < 7 && is_game_won? == false
+
+  end
+
+  def show_word_progress
+    puts @word_so_far
+  end
+
+  def valid_guess?(guess)
+    guess.length == 1 && !guessed_letters.include?(guess) && guess.match?(/^[A-Za-z]+$/)
+  end
+
+  def show_hangman_state
+    puts hangman_states[@wrong_guesses]
+  end
+
+  def update_word_status(guess)
+    (0...@selected_word.length).each do |index|
+      if @selected_word[index] == guess
+        @word_so_far[index * 2] = guess # Using * 2 because I have spaces between underscores
+      end
+    end
+  end
+
+  def is_game_won?
+    if @word_so_far.include?("_")
+      false
+    else
+      puts 'You win!'
+      true
+    end
+  end
+end
+
+class Player
+  def initialize(hangman)
+    @hangman = hangman
+    puts 'player created'
+    @hangman.play(self)
+  end
+  puts 'ah'
+  def make_guess
+    guess = gets.chomp
+    if @hangman.valid_guess?(guess)
+      if @hangman.selected_word.include?(guess)
+        @hangman.guessed_letters.push(guess)
+        puts 'Correct.'
+        @hangman.update_word_status(guess)
+      else
+        @hangman.guessed_letters.push(guess)
+        @hangman.wrong_guesses += 1
+        @hangman.show_hangman_state
+        puts "Wrong. Guesses remaining: #{7 - @hangman.wrong_guesses}"
+      end
+      @hangman.show_word_progress
+    else
+      puts "Invalid guess. Already guessed it or it's not a letter. Try again."
+    end
+  end
+end
+
+hangman = Game.new
